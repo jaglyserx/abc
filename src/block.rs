3263614c3846f4cf8daee2f592134c78367625f8
Jaglyser <joelwblomster@gmail.com>
2026-02-01 13:06:25 +0100

    added all data models needed for both the votes, certification, and blocks

--- src/block.rs
+++ src/block.rs
@@ -1,43 +1,76 @@
-// Byzantine Fault Tolerance with rotating leader: https://arxiv.org/pdf/2312.05869
-// Currently unaware of faster, or generally better algorithm's for block proposition so will
-// implement it.
-//
-// This will be an essential part of an SMR (State machine replication) protocol.
-// These ensure all nodes in the network share the same state, in an efficient manner.
-//
-// Another necessary component will be an Internet Computer Consensus (ICC) protocol,
-// which is essential to have several nodes agree on shared data.
-// Examples of these include: Proof-of-Work (PoW), Proof-of-Authority (PoA), Proof-of-Stake (PoS).
-//
-// The BANYAN SMR protocol must satisfy:
-// 1. Deadlock freeness (every sound must end with a block inclusion)
-// 2. Safety (all honest blocks should finalise the same blocks in a replica)
-// 3. Liveness (if the the current moment is perfectly sequenced, and the leader is honest, then
-//    the suggested block is added to the block tree.)
-// 4. Fast termination (if the network is momentarile synchronous, the leader is honest, and n-p
-//    replicas are honest then the block is added to the tree and finalized in one roundtrip.
-//
-// Structure needs a block proposer which handles the BANYAN algorithm.
-// To represent different types of payloads for different events we'll use a set of messages:
-// 1. Proposal block
-// 2. Vote
-// 3. Quorum certificate
-// 4. Timeout
-// 5. Timeout certificate
-struct BlockProposer {}
+use serde::{Deserialize, Serialize};
 
-impl BlockProposer {
-    pub fn new() -> BlockProposer {
-        BlockProposer {}
-    }
+pub type BlockHash = [u8; 32];
+pub type PayloadHash = [u8; 32];
+pub type NodeId = u64;
+pub type Signature = Vec<u8>;
+pub type AggregateSignature = Vec<Signature>;
+
+#[derive(Clone, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
+pub struct BlockHeader {
+    pub round: u64,
+    pub proposer: NodeId,
+    pub parent_hash: BlockHash,
+    pub payload_hash: PayloadHash,
+    pub rank: u32,
 }
 
-struct BlockData {}
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+pub struct Block {
+    pub header: BlockHeader,
+    pub payload: BlockPayload,
+    pub signature: Signature,
+}
 
-struct Block {
-    block_hash: [u8; 32],
-    parent_hash: [u8; 32],
-    height: u64,
-    finalised_at: u64,
-    data: BlockData,
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+pub struct BlockPayload {
+    pub bytes: Vec<u8>,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+pub struct NotarizationVote {
+    pub round: u64,
+    pub block_hash: BlockHash,
+    pub voter: NodeId,
+    pub signature: Signature,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+pub struct FinalizationVote {
+    pub round: u64,
+    pub block_hash: BlockHash,
+    pub voter: NodeId,
+    pub signature: Signature,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+pub struct FastVote {
+    pub round: u64,
+    pub block_hash: BlockHash,
+    pub voter: NodeId,
+    pub signature: Signature,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+pub struct NotarizationCertificate {
+    pub round: u64,
+    pub block_hash: BlockHash,
+    pub voters: Vec<NodeId>,
+    pub signatures: AggregateSignature,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+pub struct FinalizationCertificate {
+    pub round: u64,
+    pub block_hash: BlockHash,
+    pub voters: Vec<NodeId>,
+    pub signatures: AggregateSignature,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+pub struct UnlockProof {
+    pub round: u64,
+    pub block_hash: BlockHash,
+    pub voters: Vec<NodeId>,
+    pub signatures: AggregateSignature,
 }
